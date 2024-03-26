c2ed8a674221d29a1c6e05bfdcf7365700b07d42
AdityaPatil-R <adityapatil@g.ucla.edu>
2024-03-25 22:01:41 -0700

    Initial Commit

---
+++ Project-6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project-6
+//
+//  Created by Aditya Patil on 3/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
